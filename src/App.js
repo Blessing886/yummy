b3e38afd9681a48613310bde205c955bbbeb2e44
BlessingOkora <moraablessing082@gmail.com>
2024-11-14 13:53:49 +0300

    fixes navbar styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
     <div className='App'>
       <Main />
       <div>
-        <p className="main-content">YUM</p>
+        <p className="main-content">Unleash Your Inner Chef</p>
+        <div className="main-image">
+        <img src="https://media.istockphoto.com/id/1996314747/photo/woman-smelling-food-cooking-in-a-pot-on-her-kitchen-stove.webp?a=1&b=1&s=612x612&w=0&k=20&c=R8gkqEreyZH4YPuZfNxIPhm8kJwKGzP1ls7COcCTySs="
+        alt="lady cooking" className="image-item" />
+        <img src="https://plus.unsplash.com/premium_photo-1728412897938-d70e9c5becd7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJlY2lwZXN8ZW58MHx8MHx8fDA%3D"
+        alt="prep" className="image-item" />
+        </div>
       </div>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,12 @@ function Navbar() {
   return (
     <nav>
         <h1>YUMMY!</h1>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/popular-categories">Popular Categories</Link></li>
-            <li><Link to="/super-delicious">Super Delicious</Link></li>
-            <li><Link to="/form-submit">Submit Recipe</Link></li>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/popular-categories">Popular Categories</Link></li>
+          <li><Link to="/super-delicious">Super Delicious</Link></li>
+          <li><Link to="/form-submit">Submit Recipe</Link></li>
+        </ul>
     </nav>
   )
 }
